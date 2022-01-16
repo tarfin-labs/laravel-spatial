910ecc9c8ef0c72941dd2c4cd500b711530413ee
Turan Karatuğ <tkaratug@hotmail.com.tr>
2022-01-16 19:05:37 +0300

    srid param added to all scopes in HasSpatial trait.

--- src/Traits/HasSpatial.php
+++ src/Traits/HasSpatial.php
@@ -4,6 +4,7 @@ namespace TarfinLabs\LaravelSpatial\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
+use TarfinLabs\LaravelSpatial\Casts\LocationCast;
 use TarfinLabs\LaravelSpatial\Types\Point;
 
 trait HasSpatial
@@ -15,35 +16,38 @@ trait HasSpatial
         }
 
         $query->selectRaw("ST_Distance(
-            ST_SRID({$column}, 4326),
-            ST_SRID(Point(?, ?), 4326)
+            ST_SRID({$column}, {$point->getSrid()}),
+            ST_SRID(Point(?, ?), {$point->getSrid()})
         ) as distance", [$point->getLng(), $point->getLat()]);
     }
 
     public function scopeWithinDistanceTo(Builder $query, string $column, Point $point, int $distance): void
     {
         $query->whereRaw("ST_Distance(
-            ST_SRID({$column}, 4326),
-            ST_SRID(Point(?, ?), 4326)
+            ST_SRID({$column}, {$point->getSrid()}),
+            ST_SRID(Point(?, ?), {$point->getSrid()})
         ) <= ?", [...[$point->getLng(), $point->getLat()], $distance]);
     }
 
-    public function scopeOrderByDistanceTo(Builder $query, Point $point, string $direction = 'asc'): void
+    public function scopeOrderByDistanceTo(Builder $query, string $column, Point $point, string $direction = 'asc'): void
     {
         $direction = strtolower($direction) === 'asc' ? 'asc' : 'desc';
 
-        $query->orderByRaw('ST_Distance(
-            ST_SRID(location, 4326),
-            ST_SRID(Point(?, ?), 4326)
-        )'.$direction, [$point->getLng(), $point->getLat()]);
+        $query->orderByRaw("ST_Distance(
+            ST_SRID({$column}, {$point->getSrid()}),
+            ST_SRID(Point(?, ?), {$point->getSrid()})
+        )".$direction, [$point->getLng(), $point->getLat()]);
     }
 
     public function newQuery(bool $excludeDeleted = true): Builder
     {
         $raw = '';
-        foreach ($this->geometry as $column) {
-            $raw .= " ST_AsText({$this->getTable()}.{$column}) as {$column}, ";
+        $casts = collect($this->getCasts())->filter(fn ($cast) => $cast === LocationCast::class);
+
+        foreach ($casts as $column => $cast) {
+            $raw .= " CONCAT(ST_AsText({$this->getTable()}.{$column}), ',', ST_SRID({$this->getTable()}.{$column})) as {$column}, ";
         }
+
         $raw = substr($raw, 0, -2);
 
         return parent::newQuery($excludeDeleted)->addSelect('*', DB::raw($raw));
