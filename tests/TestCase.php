ccc503618a8a48a57fa08a3d143c49b84d48ec5b
Turan Karatuğ <tkaratug@hotmail.com.tr>
2022-01-18 16:09:51 +0300

    Migration added for tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,23 @@ use TarfinLabs\LaravelSpatial\LaravelSpatialServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    protected function defineDatabaseMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+    }
+
+    protected function defineEnvironment($app): void
+    {
+        $app['config']->set('database.default', 'mysql');
+        $app['config']->set('database.connections.mysql', [
+            'driver'    => 'mysql',
+            'host'      => '127.0.0.1',
+            'port'      => 3306,
+            'database'  => 'laravel_spatial_test',
+            'username'  => 'root',
+        ]);
+    }
+
     protected function getPackageProviders($app): array
     {
         return [
---
+++ tests/database/migrations/0000_00_00_000000_create_addresses_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->point('location');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
