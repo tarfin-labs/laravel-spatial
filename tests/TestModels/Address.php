4d13c49c3f292f8f14dbbed4ab59a54c91b04a61
Yunus Emre Deligöz <ye@deligoz.me>
2022-01-20 10:50:37 +0300

    Address: Add property and method docblocks

--- tests/TestModels/Address.php
+++ tests/TestModels/Address.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace TarfinLabs\LaravelSpatial\Tests\TestModels;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use TarfinLabs\LaravelSpatial\Casts\LocationCast;
 use TarfinLabs\LaravelSpatial\Traits\HasSpatial;
+use TarfinLabs\LaravelSpatial\Types\Point;
 
+/**
+ * Class Address
+ *
+ * @method void selectDistanceTo(Builder $query, string $column, Point $point)
+ * @method void orderByDistanceTo(Builder $query, string $column, Point $point, string $direction = 'asc')
+ * @method void withinDistanceTo(Builder $query, string $column, Point $point, int $distance)
+ *
+ * @property Point location
+ */
 class Address extends Model
 {
     use HasSpatial;
