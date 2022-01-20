fa76ace673ba84921852cf4fe59c508bcbbc3462
Yunus Emre Deligöz <ye@deligoz.me>
2022-01-20 10:51:33 +0300

    Small test refactors

--- tests/PointTest.php
+++ tests/PointTest.php
@@ -12,27 +12,27 @@ class PointTest extends TestCase
     /** @test */
     public function it_sets_lat_lng_and_srid_in_constructor(): void
     {
-        // Arrange
+        // 1. Arrange
         $lat = 25.1515;
         $lng = 36.1212;
         $srid = 4326;
 
-        // Act
+        // 2. Act
         $point = new Point(lat: $lat, lng: $lng, srid: $srid);
 
-        // Assert
+        // 3. Assert
         $this->assertSame(expected: $lat, actual: $point->getLat());
         $this->assertSame(expected: $lng, actual: $point->getLng());
         $this->assertSame(expected: $srid, actual: $point->getSrid());
     }
 
     /** @test */
-    public function it_returns_default_lat_lng_and_srid_if_they_are_not_given_to_constructor(): void
+    public function it_returns_default_lat_lng_and_srid_if_they_are_not_given_in_the_constructor(): void
     {
-        // Act
+        // 1. Act
         $point = new Point();
 
-        // Assert
+        // 2. Assert
         $this->assertSame(expected: 0.0, actual: $point->getLat());
         $this->assertSame(expected: 0.0, actual: $point->getLng());
         $this->assertSame(expected: 0, actual: $point->getSrid());
@@ -41,13 +41,13 @@ class PointTest extends TestCase
     /** @test */
     public function it_returns_default_srid_in_config_if_it_is_not_null(): void
     {
-        // Arrange
+        // 1. Arrange
         Config::set('laravel-spatial.default_srid', 4326);
 
-        // Act
+        // 2. Act
         $point = new Point();
 
-        // Assert
+        // 3. Assert
         $this->assertSame(expected: 0.0, actual: $point->getLat());
         $this->assertSame(expected: 0.0, actual: $point->getLng());
         $this->assertSame(expected: 4326, actual: $point->getSrid());
