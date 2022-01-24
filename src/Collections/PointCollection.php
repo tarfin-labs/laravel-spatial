5aa1d8057d4fb6947919a4906b8bf53f2de792a9
Turan Karatuğ <tkaratug@hotmail.com.tr>
2022-01-24 17:04:44 +0300

    Added PointCollection for points.

---
+++ src/Collections/PointCollection.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace TarfinLabs\LaravelSpatial\Collections;
+
+use Illuminate\Support\Collection;
+use InvalidArgumentException;
+use TarfinLabs\LaravelSpatial\Types\Point;
+
+class PointCollection extends Collection
+{
+    public function __construct(array $points = [])
+    {
+        $this->validatePoints($points);
+
+        parent::__construct($points);
+    }
+
+    public function addPoint(Point $point): void
+    {
+        $this->items[] = $point;
+    }
+
+    private function validatePoints(array $points)
+    {
+        foreach ($points as $point) {
+            if (!$point instanceof Point) {
+                throw new InvalidArgumentException(message: sprintf(
+                    '%s must be a collection of %s',
+                    get_class($this),
+                    Point::class
+                ));
+            }
+        }
+    }
+}
