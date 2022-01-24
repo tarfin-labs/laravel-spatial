1fe6990e4eba74dbba42b77ef7faae720c64053e
Turan Karatuğ <tkaratug@hotmail.com.tr>
2022-01-24 17:03:22 +0300

    Added Geometry abstract class for all spatial data types.

---
+++ src/Types/Geometry.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TarfinLabs\LaravelSpatial\Types;
+
+abstract class Geometry
+{
+    protected int $srid;
+
+    public function __construct(?int $srid)
+    {
+        $this->srid = is_null($srid)
+            ? config('laravel-spatial.default_srid') ?? 0
+            : $srid;
+    }
+
+    abstract public function getWkt(): string;
+
+    public function getSrid(): int
+    {
+        return $this->srid;
+    }
+}
